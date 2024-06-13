8758816aa7a35acc0f032f487be79a3931a45e73
Lucas Beraldi <lucas.beraldiso@gmail.com>
2024-06-13 14:03:53 -0400

    instalando bibliotecas necessarias para uso no projeto

--- contatooh/config/express.js
+++ contatooh/config/express.js
@@ -1,10 +1,27 @@
 var express = require('express');
-module.exports = function() {
-    var app = express();
-// configuração de ambiente
-    app.set('port', 3000);
+var load = require('express-load');
+var bodyParser = require('body-parser');
 
-// middleware
-    app.use(express.static('./public'));
-    return app;
+module.exports = function() {
+  var app = express();
+
+  app.set('port', 8000);
+
+  //  middlewares
+  app.use(express.static('./public'));
+  app.set('view engine', 'ejs');
+  app.set('views','./app/views');
+
+  // novos middlewares
+    app.use(bodyParser.urlencoded({extended: true}));
+    app.use(bodyParser.json());
+    app.use(require('method-override')());
+
+  // carregamento de rotas logo abaixo
+    load('models', {cwd: 'app'})
+        .then('controllers')
+        .then('routes')
+        .into(app);
+
+  return app;
 };
